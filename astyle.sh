d61b1ab8013ed859890ae9c000c0e41cdd738f82
Stephen Li <stephenli.ca@gmail.com>
2014-01-27 17:12:30 -0500

    added script to run astyle. enter 'source astyle.sh'

---
+++ astyle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+astyle \
+    --indent=spaces=4 \
+    --style=java \
+    --add-brackets \
+    --pad-oper \
+    --align-pointer=type \
+    --align-reference=name \
+    --convert-tabs \
+    --recursive \
+    ./*.c \
+    ./*.h

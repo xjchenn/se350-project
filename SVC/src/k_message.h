2915ba547b1dc3181765c188059f70db1bf4512a
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-03-05 22:08:07 -0500

    Add tracking of sending and receiving messages.
    Implemented using circular buffers. Still have to figure out printing,
    but can't be done until KCD/CRT has been dealt with.

--- SVC/src/k_message.h
+++ SVC/src/k_message.h
@@ -4,6 +4,9 @@
 #include "utils.h"
 #include "linkedlist.h"
 
+#define MSG_PREVIEW_SIZE 16
+#define MSG_BUFFER_SIZE 10
+
 typedef struct {
     node_t msg_node;
     uint32_t sender_pid;
@@ -12,6 +15,14 @@ typedef struct {
     uint32_t msg_type;
     char msg_data[1];
 } message_t;
+
+typedef struct {
+    uint32_t sender;
+    uint32_t receiver;
+    uint32_t msg_type;
+    //Add timestamp here
+    char msg_preview[16];
+} msg_hist_t;
 
 uint32_t k_send_message(uint32_t process_id, void* message_envelope);
 void* k_receive_message(int32_t* sender_id);
--- SVC/src/message.c
+++ SVC/src/message.c
@@ -5,6 +5,19 @@
 #define KERNEL_MSG_ADDR(MESSAGE) (void *)((uint32_t)MESSAGE - KERNEL_MSG_HEADER_SIZE)
 #define USER_MSG_ADDR(MESSAGE) (void *)((uint32_t)MESSAGE + KERNEL_MSG_HEADER_SIZE)
 
+msg_hist_t sent_msg_buffer[MSG_BUFFER_SIZE];
+msg_hist_t received_msg_buffer[MSG_BUFFER_SIZE];
+
+uint32_t sent_msg_buffer_size = 0;
+uint32_t received_msg_buffer_size = 0;
+
+uint32_t front_sent_msg_idx = 0;
+uint32_t end_sent_msg_idx = 0;
+
+uint32_t front_received_msg_idx = 0;
+uint32_t end_received_msg_idx = 0;
+
+
 uint32_t k_init_message(void* message, uint32_t process_id) {
     message_t* msg = (message_t *)message;
     
@@ -15,6 +28,51 @@ uint32_t k_init_message(void* message, uint32_t process_id) {
     msg->receiver_pid = process_id;
     
     return 0;
+}
+
+uint32_t copy_message_to_history(const message_t* message, msg_hist_t* history) {
+    history->sender = message->sender_pid;
+    history->receiver = message->receiver_pid;
+    history->msg_type = message->msg_type;
+    //strncpy here
+    
+    return 0;
+}
+
+void buffer_increment(uint32_t* ptr, uint32_t max_size) {
+    if(*ptr == (max_size - 1)) {
+        ptr = 0;
+    } else {
+        ptr++;
+    }
+}
+
+uint32_t track_msg(msg_hist_t* buffer, uint32_t* front, uint32_t* end, uint32_t* size, const message_t* msg) {
+    if(size == 0) {
+        copy_message_to_history(msg, &buffer[*end++]);
+        buffer_increment(end, MSG_BUFFER_SIZE);
+        *size++;
+        return 0;
+    }
+    
+    buffer_increment(end, MSG_BUFFER_SIZE);
+    copy_message_to_history(msg, &buffer[*end++]);
+    
+    if(*size < 10) {
+        *size++;
+    } else {
+        buffer_increment(front, MSG_BUFFER_SIZE);
+    }
+    
+    return 0;
+}
+
+uint32_t track_sent_msg(const message_t* msg) {
+    return track_msg(sent_msg_buffer, &front_sent_msg_idx, &end_sent_msg_idx, &sent_msg_buffer_size, msg);
+}
+
+uint32_t track_received_msg(const message_t* msg) {
+    return track_msg(received_msg_buffer, &front_received_msg_idx, &end_received_msg_idx, &received_msg_buffer_size, msg);
 }
 
 uint32_t k_send_message(uint32_t process_id, void* message_envelope) {
@@ -30,6 +88,8 @@ uint32_t k_send_message(uint32_t process_id, void* message_envelope) {
     receiver = pcbs[process_id];
     
     linkedlist_push_back(&receiver->msg_queue, &message->msg_node);
+    
+    track_sent_msg(message);
     
     if(receiver->state == MSG_BLOCKED) {
         k_pcb_msg_unblock(receiver);
@@ -56,6 +116,8 @@ void* k_receive_message(int32_t* sender_id) {
     message_node = linkedlist_pop_front(&current_pcb->msg_queue);
     message = (message_t *)message_node->value;
     *sender_id = message->sender_pid;
+    
+    track_received_msg(message);
     
     return (void*)USER_MSG_ADDR(message);
 }

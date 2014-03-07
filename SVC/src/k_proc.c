7c2a2f48c59e11df1be0ae3f882e05b5d6d61036
= <=>
2014-03-07 17:57:40 -0500

    Fix printing again...

--- SVC/src/k_proc.c
+++ SVC/src/k_proc.c
@@ -77,6 +77,7 @@ void kcd_proc(void) {
         if(msg->msg_type == DEFAULT) {
             msg->msg_type = CRT_DISPLAY;
             send_message(PID_CRT, msg);
+						continue;
         } else if(msg->msg_type == KCD_REG) {
             if(num_of_cmds_reg == 10) {
 							goto cleanup;
